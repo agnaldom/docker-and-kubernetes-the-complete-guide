16dbffe8b31be7e606930d59d9d6bbc38b9dcb16
Victor Faria <victor.faria@vivitech.com.br>
2019-06-16 20:05:15 -0300

    Adicionando o Dockerfile para client

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -5,21 +5,25 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-        <Link to="/">Home</Link>
-        <Link to="/otherpage">Other Page</Link>
-      </header>
-      <div>
-        <Route exact path="/" comopnent={Fib} />
-        <Route path="/otherpage" component={OtherPage} />
-      </div>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" comopnent={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
